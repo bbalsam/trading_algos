a1f143591043d783fc3d7e76b3af246b1214564e
bbalsam <43484084+bbalsam@users.noreply.github.com>
2022-12-21 11:02:27 -0600

    Update MomentumStrategy.sql
    Change to startdate variable function.

--- MomentumStrategy.sql
+++ MomentumStrategy.sql
@@ -2,6 +2,7 @@
 use stock
 
 DECLARE @inputdate DATE
+DECLARE @volumeMin BIGINT
 DECLARE @yearpriordate DATE
 DECLARE @halfyearpriordate DATE
 DECLARE @quarterpriordate DATE
@@ -10,7 +11,8 @@ DECLARE @startdate DATE
 DECLARE @endpulldate DATE
 DECLARE @enddate DATE
 
-SET @inputdate = '12/19/2021'
+SET @inputdate = '11/01/2022'
+SET @volumeMin = 25000000
 
 /*SET @inputdate = (SELECT MAX(dtDate)
 				FROM
@@ -25,7 +27,7 @@ SET @pulldate = (SELECT MAX(dtDate)
 					strTick = 'SPY'
 					AND dtDate <@inputdate)
 
-SET @startdate = (SELECT MAX(dtDate)
+SET @startdate = (SELECT MIN(dtDate)
 				FROM
 					STOCKS
 				WHERE
@@ -80,13 +82,28 @@ SELECT
 FROM
 	stocks s
 JOIN
-	IndexStocks ins ON ins.Symbol = s.strTick
+	(SELECT
+		strtick
+		,avg(intVol) AS AVGVol
+	FROM
+		stocks
+	WHERE
+		dtDate >='01/01/2022'
+		AND strTick not like '%-%'
+		AND strTick not like '%^%'
+		AND strTick not like '%=%'
+	GROUP BY
+		strtick
+	HAVING
+		AVG(intvol) > @volumeMin
+	) ins ON ins.strtick = s.strtick
+	--IndexStocks ins ON ins.Symbol = s.strTick
 WHERE
 	--strTick in ('A','AAL','AAP','AAPL','ABBV','ABC','ABMD','ABNB','ABT','ACN','ADBE','ADI','ADM','ADP','ADSK','AEE','AEP','AES','AFL','AIG','AIZ','AJG','AKAM','ALB','ALGN','ALK','ALL','ALLE','AMAT','AMCR','AMD','AME','AMGN','AMP','AMT','AMZN','ANET','ANSS','AON','AOS','APA','APD','APH','APTV','ARE','ATO','ATVI','AVB','AVGO','AVY','AWK','AXP','AZO','BA','BAC','BAX','BBY','BDX','BEN','BIIB','BK','BKNG','BKR','BLK','BMY','BR','BSX','BWA','BXP','C','CAG','CAH','CARR','CAT','CB','CBOE','CBRE','CCI','CCL','CDNS','CDW','CE','CF','CFG','CHD','CHRW','CHTR','CI','CINF','CL','CLX','CMA','CMCSA','CME','CMG','CMI','CMS','CNC','CNP','COF','COIN','COO','COP','COST','CPB','CPRT','CRM','CRWD','CSCO','CSX','CTAS','CTSH','CTVA','CVS','CVX','D','DAL','DD','DE','DFS','DG','DGX','DHI','DHR','DIS','DISH','DLR','DLTR','DOV','DOW','DPZ','DRI','DTE','DUK','DVA','DVN','DXC','DXCM','EA','EBAY','ECL','ED','EFX','EIX','EL','EMN','EMR','ENPH','EOG','EQIX','EQR','ES','ESS','ETHE','ETN','ETR','ETSY','EVRG','EW','EXC','EXPD','EXPE','EXR','F','FANG','FAST','FBHS','FCX','FDX','FE','FFIV','FIS','FISV','FITB','FLT','FMC','FOX','FOXA','FRC','FRT','FTNT','FTV','GBTC','GD','GE','GILD','GIS','GL','GLD','GLW','GM','GOOG','GOOGL','GPC','GPN','GRMN','GS','GWW','HAL','HAS','HBAN','HCA','HD','HES','HIG','HII','HLT','HOG','HOLX','HON','HPE','HPQ','HRB','HRL','HSIC','HST','HSY','HUM','HWM','IBM','ICE','IDXX','IEX','IFF','ILMN','INCY','INTC','INTU','IP','IPG','IQV','IR','IRM','ISRG','IT','ITW','IVZ','J','JBHT','JCI','JKHY','JNJ','JNPR','JPM','K','KEY','KEYS','KHC','KIM','KLAC','KMB','KMI','KMX','KO','KR','L','LDOS','LEN','LH','LHX','LIN','LKQ','LLY','LMT','LNC','LNT','LOW','LRCX','LULU','LUV','LVS','LW','LYB','LYV','MA','MAA','MAR','MAS','MCD','MCHP','MCK','MCO','MDLZ','MDT','MET','MGM','MHK','MKC','MKTX','MLM','MMC','MMM','MNST','MO','MOS','MPC','MRK','MRO','MS','MSCI','MSFT','MSI','MTB','MTD','MU','NCLH','NDAQ','NEE','NEM','NFLX','NI','NKE','NOC','NOW','NRG','NSC','NTAP','NTRS','NUE','NVDA','NVR','NWL','NWS','NWSA','O','ODFL','OKE','OMC','ORCL','ORLY','OTIS','OXY','PAYC','PAYX','PCAR','PEAK','PEG','PEP','PFE','PFG','PG','PGR','PH','PHM','PKG','PKI','PLD','PM','PNC','PNR','PNW','PPG','PPL','PRU','PSA','PSX','PTON','PWR','PXD','PYPL','QCOM','QRVO','RCL','RE','REG','REGN','RF','RHI','RJF','RL','RMD','ROK','ROL','ROP','ROST','RSG','RTX','SBAC','SBUX','SCHW','SEDG','SEE','SHW','SIVB','SJM','SLB','SLV','SNA','SNPS','SO','SPCE','SPG','SPGI','SPXL','SPY','SRE','STE','STT','STX','STZ','SWK','SWKS','SYF','SYK','SYY','T','TAP','TDG','TEL','TER','TFC','TFX','TGT','TJX','TMO','TMUS','TPR','TQQQ','TROW','TRV','TSCO','TSLA','TSN','TT','TTWO','TXN','TXT','UAL','UDR','UHS','ULTA','UNH','UNP','UPS','URI','USB','V','VFC','VLO','VMC','VNO','VRSK','VRSN','VRTX','VTR','VZ','WAB','WAT','WBA','WDC','WEC','WELL','WFC','WHR','WM','WMB','WMT','WRB','WRK','WST','WY','WYNN','XEL','XLB','XLC','XLE','XLF','XLI','XLK','XLP','XLRE','XLU','XLV','XLY','XOM','XRAY','XRX','XYL','YUM','ZBH','ZBRA','ZION','ZM','ZTS')
 	--AND strTick NOT IN('ACGL','TRGP','CSGP','INVH','KDP','ON','VICI','CPT','MOH','NDSN','FDS','SBNY','SEDG','EPAM','BRO','CDAY','MTCH','TECH','MRNA','OGN','CRL','PTC','CZR','GNRC','NXPI','MPWR','TRMB','ENPH','TSLA','POOL','CTLT','ETSY','BIO','TDY','TYL','WST','DPZ','DXCM','CARR','OTIS','HWM','PAYC','LYV','STE','ZBRA','ODFL','WRB','NOW','LVS','NVR','CDW','IEX','LDOS','TMUS','MKTX','AMCR','CTVA','WAB','ATO','TFX','FRC')
 	--AND
 	s.dtDate in (@pulldate,@startdate,@quarterpriordate,@halfyearpriordate,@yearpriordate,@enddate,@endpulldate)
-	AND ins.date_first_added <= @startdate
+	--AND ins.date_first_added <= @startdate
 )
 SELECT TOP 10
 	strTick
